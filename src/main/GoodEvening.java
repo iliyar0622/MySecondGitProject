4341f8f6e0b233bfb5b953f9e64b4ab7e63eb85d
iliyar0622 <iliyar0622@gmail.com>
2018-03-10 12:21:21 -0500

    added print statement

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -3,5 +3,8 @@ package main;
 public class GoodEvening {
  public static void main(String[]args) {
 	 System.out.println("Good Evening Class");
+    System.out.println("My name is bala");
+ 
+ 
  }
 }
